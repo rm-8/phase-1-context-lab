91042fb840d3b77dd9797a0de349f101d8e9b359
rm-8 <mohamedridhwan8@gmail.com>
2024-01-15 10:53:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -21,3 +21,73 @@ const allWagesFor = function () {
     return payable
 }
 
+// createEmployeeRecord
+function createEmployeeRecord(infoArray) {
+    return {
+      firstName: infoArray[0],
+      familyName: infoArray[1],
+      title: infoArray[2],
+      payPerHour: infoArray[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  // createEmployeeRecords
+  function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+  }
+  
+  // createTimeInEvent
+  function createTimeInEvent(dateTime) {
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(dateTime.slice(-4), 10),
+      date: dateTime.split(" ")[0],
+    });
+    return this;
+  }
+  
+  // createTimeOutEvent
+  function createTimeOutEvent(dateTime) {
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(dateTime.slice(-4), 10),
+      date: dateTime.split(" ")[0],
+    });
+    return this;
+  }
+  
+  // hoursWorkedOnDate
+  function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find((event) => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find((event) => event.date === date);
+  
+    if (timeInEvent && timeOutEvent) {
+      return (timeOutEvent.hour - timeInEvent.hour) / 100;
+    }
+  
+    return 0;
+  }
+  
+  // wagesEarnedOnDate
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+  }
+  
+  // allWagesFor
+  // Placeholder for your code. You can use the provided code here.
+  
+  // findEmployeeByFirstName
+  function findEmployeeByFirstName(sourceArray, firstName) {
+    return sourceArray.find((employee) => employee.firstName === firstName);
+  }
+  
+  // calculatePayroll
+  function calculatePayroll(employees) {
+    return employees.reduce(
+      (totalPayroll, employee) => totalPayroll + allWagesFor.call(employee),
+      0
+    );
+  }
